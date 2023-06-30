bd8d388ccf7c4080d099e9c65d5c662f38d5ddc4
guqing <i@guqing.email>
2023-06-30 12:45:29 +0800

    feat: support page matching to use code highlighting

--- src/main/java/run/halo/highlightjs/HighlightJSHeadProcessor.java
+++ src/main/java/run/halo/highlightjs/HighlightJSHeadProcessor.java
@@ -1,16 +1,32 @@
 package run.halo.highlightjs;
 
+import com.google.common.base.Throwables;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
+import org.springframework.util.PropertyPlaceholderHelper;
+import org.springframework.util.RouteMatcher;
+import org.springframework.web.util.pattern.PathPatternRouteMatcher;
+import org.springframework.web.util.pattern.PatternParseException;
+import org.thymeleaf.context.Contexts;
 import org.thymeleaf.context.ITemplateContext;
 import org.thymeleaf.model.IModel;
 import org.thymeleaf.model.IModelFactory;
 import org.thymeleaf.processor.element.IElementModelStructureHandler;
+import org.thymeleaf.web.IWebRequest;
 import reactor.core.publisher.Mono;
 import run.halo.app.plugin.ReactiveSettingFetcher;
 import run.halo.app.theme.dialect.TemplateHeadProcessor;
+
+import java.util.List;
+import java.util.Properties;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.apache.commons.lang3.StringUtils.defaultIfBlank;
 
 /**
  * @author ryanwang
@@ -23,16 +39,25 @@ public class HighlightJSHeadProcessor implements TemplateHeadProcessor {
     private static final String TEMPLATE_ID_VARIABLE = "_templateId";
 
     private final ReactiveSettingFetcher reactiveSettingFetcher;
+    private final PathPatternRouteMatcher routeMatcher = new PathPatternRouteMatcher();
 
     @Override
     public Mono<Void> process(ITemplateContext context, IModel model, IElementModelStructureHandler structureHandler) {
-        if (!isContentTemplate(context)) {
-            return Mono.empty();
-        }
-        return reactiveSettingFetcher.fetch("basic", BasicConfig.class).doOnNext(basicConfig -> {
-            final IModelFactory modelFactory = context.getModelFactory();
-            model.add(modelFactory.createText(highlightJsScript(basicConfig.getExtra_languages(), basicConfig.getStyle())));
-        }).doOnError((e) -> log.warn("HighlightJSHeadProcessor process failed", e)).then();
+        return reactiveSettingFetcher.fetch("basic", BasicConfig.class)
+                .doOnNext(basicConfig -> {
+                    if (mismatchedRoute(context, basicConfig) && notContentTemplate(context)) {
+                        return;
+                    }
+
+                    final IModelFactory modelFactory = context.getModelFactory();
+                    String scriptString = highlightJsScript(basicConfig.getExtra_languages(), basicConfig.getStyle());
+                    model.add(modelFactory.createText(scriptString));
+                })
+                .onErrorResume(e -> {
+                    log.error("HighlightJSHeadProcessor process failed", Throwables.getRootCause(e));
+                    return Mono.empty();
+                })
+                .then();
     }
 
     private String highlightJsScript(String extraLanguages, String style) {
@@ -74,14 +99,47 @@ public class HighlightJSHeadProcessor implements TemplateHeadProcessor {
                 """.formatted(style, extraLanguages != null ? extraLanguages : "");
     }
 
-    public boolean isContentTemplate(ITemplateContext context) {
-        return "post".equals(context.getVariable(TEMPLATE_ID_VARIABLE))
-                || "page".equals(context.getVariable(TEMPLATE_ID_VARIABLE));
+    public boolean notContentTemplate(ITemplateContext context) {
+        return !"post".equals(context.getVariable(TEMPLATE_ID_VARIABLE))
+                && !"page".equals(context.getVariable(TEMPLATE_ID_VARIABLE));
+    }
+
+    public boolean mismatchedRoute(ITemplateContext context, BasicConfig basicConfig) {
+        if (!Contexts.isWebContext(context)) {
+            return false;
+        }
+        IWebRequest request = Contexts.asWebContext(context).getExchange().getRequest();
+        String requestPath = request.getRequestPath();
+        RouteMatcher.Route requestRoute = routeMatcher.parseRoute(requestPath);
+
+        return basicConfig.nullSafeRules()
+                .stream()
+                .noneMatch(rule -> isMatchedRoute(requestRoute, rule));
+    }
+
+    private boolean isMatchedRoute(RouteMatcher.Route requestRoute, PathMatchRule rule) {
+        try {
+            return routeMatcher.match(rule.getPathPattern(), requestRoute);
+        } catch (PatternParseException e) {
+            // ignore
+            log.warn("Parse route pattern [{}] failed", rule.getPathPattern(), Throwables.getRootCause(e));
+        }
+        return false;
+    }
+
+    @Data
+    public static class PathMatchRule {
+        private String pathPattern;
     }
 
     @Data
     public static class BasicConfig {
         String extra_languages;
         String style;
+        List<PathMatchRule> rules;
+
+        public List<PathMatchRule> nullSafeRules() {
+            return ObjectUtils.defaultIfNull(rules, List.of());
+        }
     }
 }
