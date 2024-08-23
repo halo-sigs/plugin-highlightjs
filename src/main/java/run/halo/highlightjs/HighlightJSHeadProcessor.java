415b5018f31d6f9a7f30c5d6aae4e5b6aef7c497
Ryan Wang <i@ryanc.cc>
2024-08-23 17:24:50 +0800

    Add enableThemeSideRender option
    Signed-off-by: Ryan Wang <i@ryanc.cc>

--- src/main/java/run/halo/highlightjs/HighlightJSHeadProcessor.java
+++ src/main/java/run/halo/highlightjs/HighlightJSHeadProcessor.java
@@ -39,13 +39,17 @@ public class HighlightJSHeadProcessor implements TemplateHeadProcessor {
     private final ReactiveSettingFetcher reactiveSettingFetcher;
 
     private final RouteMatcher routeMatcher = createRouteMatcher();
-    
+
     private final TemplateEngine templateEngine = new SpringTemplateEngine();
 
     @Override
     public Mono<Void> process(ITemplateContext context, IModel model, IElementModelStructureHandler structureHandler) {
         return reactiveSettingFetcher.fetch("basic", BasicConfig.class)
                 .doOnNext(basicConfig -> {
+                    if (!basicConfig.enableThemeSideRender) {
+                        return;
+                    }
+
                     if (mismatchedRoute(context, basicConfig) && notContentTemplate(context)) {
                         return;
                     }
@@ -64,30 +68,32 @@ public class HighlightJSHeadProcessor implements TemplateHeadProcessor {
     private String highlightJsScript(BasicConfig basicConfig) {
         var context = new Context();
         context.setVariable("config", basicConfig);
-        var code = templateEngine.process("""
-                <!-- PluginHighlightJS start -->
-                <link th:href="|/plugins/PluginHighlightJS/assets/static/styles/${config.style}|" rel="stylesheet"/>
-                <script defer src="/plugins/PluginHighlightJS/assets/static/highlight.min.js"></script>
+        var code = templateEngine.process(
+                """
+                        <!-- PluginHighlightJS start -->
+                        <link th:href="|/plugins/PluginHighlightJS/assets/static/styles/${config.style}|" rel="stylesheet"/>
+                        <script defer src="/plugins/PluginHighlightJS/assets/static/highlight.min.js"></script>
 
-                <th:block th:if="${config.showCopyButton}">
-                    <link href="/plugins/PluginHighlightJS/assets/static/plugins/highlightjs-copy.min.css" rel="stylesheet"/>
-                    <script defer src="/plugins/PluginHighlightJS/assets/static/plugins/highlightjs-copy.min.js"></script>
-                </th:block>
+                        <th:block th:if="${config.showCopyButton}">
+                            <link href="/plugins/PluginHighlightJS/assets/static/plugins/highlightjs-copy.min.css" rel="stylesheet"/>
+                            <script defer src="/plugins/PluginHighlightJS/assets/static/plugins/highlightjs-copy.min.js"></script>
+                        </th:block>
 
-                <link href="/plugins/PluginHighlightJS/assets/static/plugins/override.css" rel="stylesheet"/>
+                        <link href="/plugins/PluginHighlightJS/assets/static/plugins/override.css" rel="stylesheet"/>
 
-                <script th:inline="javascript">
-                document.addEventListener("DOMContentLoaded", function () {
-                    [# th:if="${config.showCopyButton}"]
-                        hljs.addPlugin(new CopyButtonPlugin({ lang: "zh"}));
-                    [/]
-                    document.querySelectorAll("pre code").forEach((el) => {
-                        hljs.highlightElement(el);
-                    });
-                });
-                </script>
-                <!-- PluginHighlightJS end -->
-                """, context);
+                        <script th:inline="javascript">
+                        document.addEventListener("DOMContentLoaded", function () {
+                            [# th:if="${config.showCopyButton}"]
+                                hljs.addPlugin(new CopyButtonPlugin({ lang: "zh"}));
+                            [/]
+                            document.querySelectorAll("pre code").forEach((el) => {
+                                hljs.highlightElement(el);
+                            });
+                        });
+                        </script>
+                        <!-- PluginHighlightJS end -->
+                        """,
+                context);
         return code;
     }
 
@@ -132,6 +138,7 @@ public class HighlightJSHeadProcessor implements TemplateHeadProcessor {
 
     @Data
     public static class BasicConfig {
+        boolean enableThemeSideRender;
         String style;
         List<PathMatchRule> rules;
         boolean showCopyButton;
