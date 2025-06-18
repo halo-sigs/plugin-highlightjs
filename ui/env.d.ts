4261c8aa9c984d6f31ec48b21ef65a9d9cc946f0
Ryan Wang <i@ryanc.cc>
2025-06-18 18:00:57 +0800

    refactor: simplify bundler config
    Signed-off-by: Ryan Wang <i@ryanc.cc>

--- ui/env.d.ts
+++ ui/env.d.ts
@@ -1,2 +1,1 @@
-/// <reference types="vite/client" />
-/// <reference types="unplugin-icons/types/vue" />
+/// <reference types="@rsbuild/core/types" />
---
+++ ui/rsbuild.config.mjs
@@ -0,0 +1,3 @@
+import { rsbuildConfig } from "@halo-dev/ui-plugin-bundler-kit";
+
+export default rsbuildConfig({});
