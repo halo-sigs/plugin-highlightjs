1cf744179e700546547d17c644d2f9dae3f31ac3
Ryan Wang <i@ryanc.cc>
2025-06-20 11:00:15 +0800

    refactor: simplify bundler config
    Signed-off-by: Ryan Wang <i@ryanc.cc>

--- ui/rsbuild.config.mjs
+++ ui/rsbuild.config.mjs
@@ -1,3 +1,24 @@
 import { rsbuildConfig } from "@halo-dev/ui-plugin-bundler-kit";
 
-export default rsbuildConfig({});
+const OUT_DIR_PROD = "../src/main/resources/console";
+const OUT_DIR_DEV = "../build/resources/main/console";
+
+export default rsbuildConfig({
+  rsbuild: ({ envMode }) => {
+    const isProduction = envMode === "production";
+    const outDir = isProduction ? OUT_DIR_PROD : OUT_DIR_DEV;
+
+    return {
+      resolve: {
+        alias: {
+          "@": "./src",
+        },
+      },
+      output: {
+        distPath: {
+          root: outDir,
+        },
+      },
+    };
+  },
+});
